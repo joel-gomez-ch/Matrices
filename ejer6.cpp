e69e4331b5b358b5e40d2d103738ecda86020b0e
joel-gomez-ch <jgomezc@unjbg.edu.pe>
2026-07-08 13:23:57 -0500

    Sexto ejercicio (suma de diagonal principal con solo 1 for)

---
+++ ejer6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    int n, suma;
+    cin >> n;
+    int matriz[n][n];
+    suma = 0;
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+        	cin >> matriz[i][j];
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        suma = suma + matriz[i][i];
+    }
+    
+    cout << suma;
+    
+    return 0;
+}
